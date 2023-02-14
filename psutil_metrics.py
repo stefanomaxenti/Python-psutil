59f854eee783db243d882a684dae13ead812a155
Stefano Maxenti <stefano.maxenti@mail.polimi.it>
2023-02-14 15:45:16 -0500

    Add files via upload

---
+++ psutil_metrics.py
@@ -0,0 +1,13 @@
+import os
+import psutil
+import time
+import datetime
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+l = pd.DataFrame()
+while True:
+    l = pd.concat([l, pd.DataFrame([[datetime.datetime.now(), psutil.cpu_percent(percpu=True)]], columns=['Date','CPU'])])
+    l.to_csv('ciao.csv')
+    time.sleep(0.5)
