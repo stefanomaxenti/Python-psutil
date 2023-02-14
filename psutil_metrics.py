08ed887908b156e6e7f5818ff204c85fc3e5a6d4
Stefano Maxenti <stefano.maxenti@mail.polimi.it>
2023-02-14 15:57:18 -0500

    Update psutil_metrics.py

--- psutil_metrics.py
+++ psutil_metrics.py
@@ -8,6 +8,6 @@ import matplotlib.pyplot as plt
 
 l = pd.DataFrame()
 while True:
-    l = pd.concat([l, pd.DataFrame([[datetime.datetime.now(), psutil.cpu_percent(percpu=True)]], columns=['Date','CPU'])])
+    l = pd.concat([l, pd.DataFrame([[datetime.datetime.now(), psutil.cpu_percent(percpu=True), psutil.virtual_memory()]], columns=['Date','CPU', 'RAM'])])
     l.to_csv('output.csv')
     time.sleep(0.5)
